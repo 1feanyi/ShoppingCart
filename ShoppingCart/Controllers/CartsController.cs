9613487efb1fbc5a315266a29588cf1dc3097647
1feanyi <ifeanyi.igbo@gmail.com>
2015-11-16 17:22:07 -0500

    Building Cart Summary

---
+++ ShoppingCart/Controllers/CartsController.cs
@@ -0,0 +1,45 @@
+﻿using ShoppingCart.Models;
+using ShoppingCart.Services;
+using ShoppingCart.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingCart.Controllers
+{
+    public class CartsController : Controller
+    {
+        private readonly CartService _cartService = new CartService();
+
+        public CartsController()
+        {
+            AutoMapper.Mapper.CreateMap<Cart, CartViewModel>();
+            AutoMapper.Mapper.CreateMap<CartItem, CartItemViewModel>();
+            AutoMapper.Mapper.CreateMap<Book, BookViewModel>();
+            AutoMapper.Mapper.CreateMap<Author, AuthorViewModel>();
+            AutoMapper.Mapper.CreateMap<Category, CategoryViewModel>();
+        }
+
+        // This ChildActionOnly attribute, in combination with the layout loading this method via 
+        // the HtmlHelper and the Action method, lets me use the MVC framework 
+        // without the overhead of a full request.
+        [ChildActionOnly]
+        public PartialViewResult Summary()
+        {
+            var cart = _cartService.GetBySessionId(HttpContext.Session.SessionID);
+
+            return PartialView(AutoMapper.Mapper.Map<Cart, CartViewModel>(cart));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _cartService.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
